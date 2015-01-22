42203ec448b81b695febd8ed444932994f72c74e
David Pacheco <david.pacheco@beubi.com>
2015-01-22 12:31:04 +0000

    IMFQE-127

--- src/Beubi/DemoBundle/Controller/DefaultController.php
+++ src/Beubi/DemoBundle/Controller/DefaultController.php
@@ -9,10 +9,64 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/", name="home")
      * @Template()
      */
     public function indexAction()
+    {
+        return array();
+    }
+
+    /**
+     * @Route("/contact", name="contacts")
+     * @Template("BeubiDemoBundle:Default:contact.html.twig")
+     */
+    public function contactsAction()
+    {
+        return array();
+    }
+
+    /**
+     * @Route("/about", name="about")
+     * @Template("BeubiDemoBundle:Default:about.html.twig")
+     */
+    public function aboutAction()
+    {
+        return array();
+    }
+
+    /**
+     * @Route("/sidebar/left", name="sidebar_left")
+     * @Template("BeubiDemoBundle:Default:sidebar-left.html.twig")
+     */
+    public function sidebarLeftAction()
+    {
+        return array();
+    }
+
+    /**
+     * @Route("/sidebar/right", name="sidebar_right")
+     * @Template("BeubiDemoBundle:Default:sidebar-right.html.twig")
+     */
+    public function sidebarRightAction()
+    {
+        return array();
+    }
+
+    /**
+     * @Route("/signin", name="signin")
+     * @Template("BeubiDemoBundle:Default:signin.html.twig")
+     */
+    public function signinAction()
+    {
+        return array();
+    }
+
+    /**
+     * @Route("/signup", name="signup")
+     * @Template("BeubiDemoBundle:Default:signup.html.twig")
+     */
+    public function signupAction()
     {
         return array();
     }
