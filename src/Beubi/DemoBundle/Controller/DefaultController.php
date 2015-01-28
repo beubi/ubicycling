ee2dcec1ea9da91b7736668c986cb65e96c46a10
David Pacheco <david.pacheco@beubi.com>
2015-01-28 14:34:43 +0000

    IMFQE-156

--- src/Beubi/DemoBundle/Controller/DefaultController.php
+++ src/Beubi/DemoBundle/Controller/DefaultController.php
@@ -14,7 +14,7 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
-        return $this->redirect($this->generateUrl('demo_form', array()));
+        return $this->redirect($this->generateUrl('workouts', array()));
     }
 
     /**
@@ -72,21 +72,133 @@ class DefaultController extends Controller
     }
 
     /**
-     * @Route("/demo-form", name="demo_form")
-     * @Template("BeubiDemoBundle:Default:demo-form.html.twig")
+     * @Route("/workouts", name="workouts")
+     * @Template("BeubiDemoBundle:Default:workouts.html.twig")
      */
-    public function demoFormAction()
+    public function workoutsAction()
     {
         $table_contents = array();
 
-        for ($i=0; $i < 100; $i++) {
-            $item = array('id' => $i+1, 'description' => 'A, quibusdam, nobis, eveniet consequatur alias doloremque
-            officia blanditiis fuga et numquam labore reiciendis voluptas quis repellat quos sunt non dolore consectetur at sit nam tenetur dolorem',
-            'name' => 'Quibusdam');
+        $item = array(
+            'title' => 'Easy Workout',
+            'distance' => 53,
+            'date' => '27/01/2015 08:00',
+            'duration' => '1:51:00',
+            'description' => 'Easy recovery ride.');
+        array_push($table_contents, $item);
 
-            array_push($table_contents, $item);
-        }
+        $item = array(
+            'title' => 'Endurance Ride',
+            'distance' => 112,
+            'date' => '28/01/2015 09:00',
+            'duration' => '3:51:00',
+            'description' => 'Endurance workout with some climbing and some short sprints.');
+        array_push($table_contents, $item);
 
-        return array('table' => $table_contents);
+        $item = array(
+            'title' => 'Force Workout',
+            'distance' => 63,
+            'date' => '29/01/2015 09:30',
+            'duration' => '2:11:00',
+            'description' => 'Cadence variations workout, with 3x7´at (L3 60 rpm + L3/4 > 90rpm)');
+        array_push($table_contents, $item);
+
+        $item = array(
+            'title' => 'Easy Workout',
+            'distance' => 45,
+            'date' => '30/01/2015 09:00',
+            'duration' => '1:31:00',
+            'description' => 'Easy recovery ride.');
+        array_push($table_contents, $item);
+
+        $item = array(
+            'title' => 'VO2max',
+            'distance' => 53,
+            'date' => '31/01/2015 09:30',
+            'duration' => '1:59:00',
+            'description' => 'VO2max intervals - 3x3´at 390W with 3´ rec.');
+        array_push($table_contents, $item);
+
+        $item = array(
+            'title' => 'Easy Workout',
+            'distance' => 50,
+            'date' => '01/02/2015 09:00',
+            'duration' => '1:51:00',
+            'description' => 'Easy recovery ride.');
+        array_push($table_contents, $item);
+
+        $item = array(
+            'title' => 'Track Testing',
+            'distance' => 43,
+            'date' => '02/02/2015 19:00',
+            'duration' => '1:03:12',
+            'description' => 'Fartlek type workout in the velodrome.');
+        array_push($table_contents, $item);
+
+        $item = array(
+            'title' => 'Muscular Endurance',
+            'distance' => 53,
+            'date' => '03/02/2015 09:00',
+            'duration' => '2:51:00',
+            'description' => '1h L1 + 2x20´ AT4 with 10´rec. + remaining at L2.');
+        array_push($table_contents, $item);
+
+        $item = array(
+            'title' => 'Easy Workout',
+            'distance' => 45,
+            'date' => '04/02/2015 09:00',
+            'duration' => '1:31:00',
+            'description' => 'Easy recovery ride.');
+        array_push($table_contents, $item);
+
+        $item = array(
+            'title' => 'Endurance Ride',
+            'distance' => 142,
+            'date' => '05/02/2015 09:00',
+            'duration' => '5:01:00',
+            'description' => 'Endurance ride in the L2/L3 zones and some L4 in the climbs.');
+        array_push($table_contents, $item);
+
+        $item = array(
+            'title' => 'Easy Workout',
+            'distance' => 57,
+            'date' => '06/02/2015 09:00',
+            'duration' => '1:51:00',
+            'description' => 'Easy recovery ride.');
+        array_push($table_contents, $item);
+
+        $item = array(
+            'title' => 'Track Testing',
+            'distance' => 43,
+            'date' => '07/02/2015 19:00',
+            'duration' => '1:01:42',
+            'description' => 'Fartlek type workout in the velodrome.');
+        array_push($table_contents, $item);
+
+        $item = array(
+            'title' => 'Muscular Endurance',
+            'distance' => 93,
+            'date' => '08/02/2015 09:00',
+            'duration' => '3:00:10',
+            'description' => '1h L1 + 2x30´ AT4 with 10´rec. + remaining at L2.');
+        array_push($table_contents, $item);
+
+        $item = array(
+            'title' => 'Easy Workout',
+            'distance' => 40,
+            'date' => '09/02/2015 09:00',
+            'duration' => '1:31:00',
+            'description' => 'Easy recovery ride.');
+        array_push($table_contents, $item);
+
+        $item = array(
+            'title' => 'Endurance Ride',
+            'distance' => 152,
+            'date' => '10/02/2015 09:00',
+            'duration' => '5:21:00',
+            'description' => 'Endurance ride in the L2/L3 zones and some L4 in the climbs.');
+        array_push($table_contents, $item);
+
+        return array('table' => array_reverse($table_contents));
     }
 }
