edfda024f95799d9be7e20757e896762669e0ee2
David Pacheco <david.pacheco@beubi.com>
2015-03-03 17:13:40 +0000

    assetic dump db connection fix
    Conflicts:
    	composer.lock

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
 //            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Beubi\DemoBundle\BeubiDemoBundle(),
+            new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
