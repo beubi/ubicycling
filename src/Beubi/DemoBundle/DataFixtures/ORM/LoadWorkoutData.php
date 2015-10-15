fa74c4ff82a6a2f13e737a9d520acaa9a5df7979
dpacheco <david.pacheco@beubi.com>
2015-10-15 18:01:02 +0100

    IMFQE-461 - column calories

--- src/Beubi/DemoBundle/DataFixtures/ORM/LoadWorkoutData.php
+++ src/Beubi/DemoBundle/DataFixtures/ORM/LoadWorkoutData.php
@@ -50,6 +50,7 @@ class LoadWorkoutData extends AbstractFixture implements OrderedFixtureInterface
             $wk->setDescription($item['description']);
             $wk->setTimestamp(\DateTime::createFromFormat('d/m/Y H:i', $item['timestamp']));
             $wk->setDuration($item['duration']);
+            $wk->setCalories($item['calories']);
 
             return $wk;
         }
@@ -70,6 +71,7 @@ class LoadWorkoutData extends AbstractFixture implements OrderedFixtureInterface
             'distance' => 53,
             'timestamp' => '27/01/2015 08:00',
             'duration' => '1:51:00',
+            'calories' => 1101,
             'description' => 'Easy recovery ride.');
         array_push($table_contents, $item);
 
@@ -78,6 +80,7 @@ class LoadWorkoutData extends AbstractFixture implements OrderedFixtureInterface
             'distance' => 112,
             'timestamp' => '28/01/2015 09:00',
             'duration' => '3:51:00',
+            'calories' => 2754,
             'description' => 'Endurance workout with some climbing and some short sprints.');
         array_push($table_contents, $item);
 
@@ -86,6 +89,7 @@ class LoadWorkoutData extends AbstractFixture implements OrderedFixtureInterface
             'distance' => 63,
             'timestamp' => '29/01/2015 09:30',
             'duration' => '2:11:00',
+            'calories' => 1513,
             'description' => 'Cadence variations workout, with 3x7´at (L3 60 rpm + L3/4 > 90rpm)');
         array_push($table_contents, $item);
 
@@ -94,8 +98,8 @@ class LoadWorkoutData extends AbstractFixture implements OrderedFixtureInterface
             'distance' => 45,
             'timestamp' => '30/01/2015 09:00',
             'duration' => '1:31:00',
+            'calories' => 754,
             'description' => 'Easy recovery ride.');
-
         array_push($table_contents, $item);
 
         $item = array(
@@ -103,6 +107,7 @@ class LoadWorkoutData extends AbstractFixture implements OrderedFixtureInterface
             'distance' => 53,
             'timestamp' => '31/01/2015 09:30',
             'duration' => '1:59:00',
+            'calories' => 1413,
             'description' => 'VO2max intervals - 3x3´at 390W with 3´ rec.');
         array_push($table_contents, $item);
 
@@ -111,6 +116,7 @@ class LoadWorkoutData extends AbstractFixture implements OrderedFixtureInterface
             'distance' => 50,
             'timestamp' => '01/02/2015 09:00',
             'duration' => '1:51:00',
+            'calories' => 1103,
             'description' => 'Easy recovery ride.');
         array_push($table_contents, $item);
 
@@ -119,6 +125,7 @@ class LoadWorkoutData extends AbstractFixture implements OrderedFixtureInterface
             'distance' => 43,
             'timestamp' => '02/02/2015 19:00',
             'duration' => '1:03:12',
+            'calories' => 745,
             'description' => 'Fartlek type workout in the velodrome.');
         array_push($table_contents, $item);
 
@@ -127,6 +134,7 @@ class LoadWorkoutData extends AbstractFixture implements OrderedFixtureInterface
             'distance' => 53,
             'timestamp' => '03/02/2015 09:00',
             'duration' => '2:51:00',
+            'calories' => 2109,
             'description' => '1h L1 + 2x20´ AT4 with 10´rec. + remaining at L2.');
         array_push($table_contents, $item);
 
@@ -135,6 +143,7 @@ class LoadWorkoutData extends AbstractFixture implements OrderedFixtureInterface
             'distance' => 45,
             'timestamp' => '04/02/2015 09:00',
             'duration' => '1:31:00',
+            'calories' => 726,
             'description' => 'Easy recovery ride.');
         array_push($table_contents, $item);
 
@@ -143,6 +152,7 @@ class LoadWorkoutData extends AbstractFixture implements OrderedFixtureInterface
             'distance' => 142,
             'timestamp' => '05/02/2015 09:00',
             'duration' => '5:01:00',
+            'calories' => 2997,
             'description' => 'Endurance ride in the L2/L3 zones and some L4 in the climbs.');
         array_push($table_contents, $item);
 
@@ -151,6 +161,7 @@ class LoadWorkoutData extends AbstractFixture implements OrderedFixtureInterface
             'distance' => 57,
             'timestamp' => '06/02/2015 09:00',
             'duration' => '1:51:00',
+            'calories' => 981,
             'description' => 'Easy recovery ride.');
         array_push($table_contents, $item);
 
@@ -159,6 +170,7 @@ class LoadWorkoutData extends AbstractFixture implements OrderedFixtureInterface
             'distance' => 43,
             'timestamp' => '07/02/2015 19:00',
             'duration' => '1:01:42',
+            'calories' => 810,
             'description' => 'Fartlek type workout in the velodrome.');
         array_push($table_contents, $item);
 
@@ -167,6 +179,7 @@ class LoadWorkoutData extends AbstractFixture implements OrderedFixtureInterface
             'distance' => 93,
             'timestamp' => '08/02/2015 09:00',
             'duration' => '3:00:10',
+            'calories' => 1919,
             'description' => '1h L1 + 2x30´ AT4 with 10´rec. + remaining at L2.');
         array_push($table_contents, $item);
 
@@ -175,6 +188,7 @@ class LoadWorkoutData extends AbstractFixture implements OrderedFixtureInterface
             'distance' => 40,
             'timestamp' => '09/02/2015 09:00',
             'duration' => '1:31:00',
+            'calories' => 701,
             'description' => 'Easy recovery ride.');
         array_push($table_contents, $item);
 
@@ -183,8 +197,11 @@ class LoadWorkoutData extends AbstractFixture implements OrderedFixtureInterface
             'distance' => 152,
             'timestamp' => '10/02/2015 09:00',
             'duration' => '5:21:00',
+            'calories' => 3219,
             'description' => 'Endurance ride in the L2/L3 zones and some L4 in the climbs.');
         array_push($table_contents, $item);
+
+        $table_contents = array_reverse($table_contents);
 
         return $table_contents;
     }
