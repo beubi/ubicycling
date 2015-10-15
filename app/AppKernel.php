1b0ac4562f591701a500de33c64c54fec22f9cb0
dpacheco <david.pacheco@beubi.com>
2015-10-15 18:31:20 +0100

    IMFQE-461 - pagination

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Beubi\DemoBundle\BeubiDemoBundle(),
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
+            new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle (),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
