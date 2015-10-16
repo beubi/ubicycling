6abda6f12d4bbf164ce4ff325dfa4a870a019107
dpacheco <david.pacheco@beubi.com>
2015-10-16 10:22:00 +0100

    IMFQE-461 - search filters

--- src/Beubi/DemoBundle/Controller/DefaultController.php
+++ src/Beubi/DemoBundle/Controller/DefaultController.php
@@ -2,9 +2,12 @@
 
 namespace Beubi\DemoBundle\Controller;
 
+use Beubi\DemoBundle\Form\WorkoutsSearchType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
@@ -83,8 +86,64 @@ class DefaultController extends Controller
 
         $workouts = $em->getRepository('BeubiDemoBundle:Workout')->findAll();
 
+        $form = $this->createSearchForm();
+
         return array(
+            'form' => $form->createView(),
             'workouts' => $workouts,
         );
+    }
+
+    /**
+     * Search action
+     *
+     * @param \Symfony\Component\HttpFoundation\Request $request request object
+     *
+     * @Route("/workouts/search", name="workouts_search")
+     * @Method("GET")
+     * @Template("BeubiDemoBundle:Default:workouts.html.twig")
+     *
+     * @return Array request information needed for generating an output representation to the user
+     */
+    public function workoutsSearchAction(Request $request)
+    {
+        $form = $this->createSearchForm();
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $filters = $form->getData();
+
+            $queryTxt = $filters['query'];
+
+            $em = $this->getDoctrine();
+
+            $query_results = $em->getRepository('BeubiDemoBundle:Workout')->findWorkouts($queryTxt);
+
+            return array(
+                'form' => $form->createView(),
+                'workouts' => $query_results,
+            );
+        }
+        return array(
+            'form' => $form->createView(),
+            'workouts' => array(),
+        );
+
+    }
+
+    /**
+     * Creates a form to search processes
+     *
+     * @return \Symfony\Component\Form\Form
+     */
+    private function createSearchForm()
+    {
+        $form = $this->createForm(new WorkoutsSearchType(), null, array(
+            'action' => $this->generateUrl('workouts_search'), 'method' => 'GET'));
+
+        /*$form->add('search', 'submit', array('label' => 'form.buttons.search',
+            'attr' => array('class' => 'btn-sm', 'icon' => 'search')));*/
+
+        return $form;
     }
 }
